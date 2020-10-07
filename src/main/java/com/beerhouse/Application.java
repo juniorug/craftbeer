848cc904e6b4f767e33d7804b57c0bf193a22b7c
edivaldo junior <juniorug@gmail.com>
2020-10-07 10:23:43 -0300

    fixing database script

--- src/main/java/com/beerhouse/Application.java
+++ src/main/java/com/beerhouse/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.beerhouse.controllers.CraftBeerController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.beerhouse")
+//@EnableJpaRepositories("com.beerhouse.repositories")
 public class Application {
 
 	public static void main(String[] args) {
